4c568b257a9c6e46445285d401f718b63ca727e6
Brandon Phillips <phillipsbrandon@gmail.com>
2022-10-23 12:27:16 -0600

    adding function message

--- discordFn/index.js
+++ discordFn/index.js
@@ -3,7 +3,7 @@ let url =
   "https://discordapp.com/api/webhooks/1030918680911036416/04qHVB438bIct79r0SsaypfoycA5mEkqz_q_T65txri8h0ijwYEY__K_Ca5BpWJgEKXA";
 
 exports.handler = async function (event) {
-  const eventOutput = "Pipeline" + event.detail.pipeline + " " + event.detail.stage + ": " + event.detail.state
+  const eventOutput = "Pipeline" + event.detail.pipeline + " " + event.detail.stage + ": " + event.detail.state + " at: " + event.time
   const data = JSON.stringify({ content: eventOutput });
   const options = {
     hostname: "discordapp.com",
